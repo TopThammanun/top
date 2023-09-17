514e849554363f85cdb682c20ef36b931c7f62ef
SatangBudsai <mastersatang+github@gmail.com>
2023-09-17 23:23:46 +0700

    update privider theme dark mode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,24 @@
 // import RootLayout from '@/components/layouts/RootLayout'
 import { Fragment, ReactElement } from 'react'
 import { Button } from "@nextui-org/react";
-
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 type Props = {}
 
 const Home = (props: Props) => {
+  const { theme, setTheme } = useTheme()
+
   return (
     <Fragment>
-      <div className='flex justify-center items-center gap-5 p-5'>Tempalte NextJs and NextUI
+      <div className='flex justify-center items-center gap-5 p-5 '>Tempalte NextJs and NextUI
         <Button color="primary">
           Button
         </Button>
+      </div>
+      <div className='p-5 rounded-lg border-2'>
+        The current theme is: {theme}
+        <Button onClick={() => setTheme('light')}>Light Mode</Button>
+        <Button onClick={() => setTheme('dark')}>Dark Mode</Button>
       </div>
     </Fragment >
   )
--- src/providers/theme/index.tsx
+++ src/providers/theme/index.tsx
@@ -1,11 +1,14 @@
 'use client'
 
 import { NextUIProvider } from '@nextui-org/react'
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
     return (
         <NextUIProvider>
-            {children}
+            <NextThemesProvider attribute="class" defaultTheme="light">
+                {children}
+            </NextThemesProvider>
         </NextUIProvider>
     )
 }
