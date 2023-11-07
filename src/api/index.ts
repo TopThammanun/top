26c0da77315c39d66b792fb515fb9bc20588c383
SatangBudsai <mastersatang+github@gmail.com>
2023-11-07 10:00:59 +0700

    update alert when api error

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,4 @@
+import Alert from "@/components/ui/alert";
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 import $ from "jquery";
 
@@ -16,7 +17,7 @@ export class ServerError extends Error {
   }
 }
 
-const service = axios.create({
+const apiBase = axios.create({
   baseURL,
   withCredentials: true,
   headers: {
@@ -25,17 +26,20 @@ const service = axios.create({
   },
 });
 
-service.interceptors.request.use((config) => {
+apiBase.interceptors.request.use((config) => {
   return config;
 });
 
-service.interceptors.response.use(
+apiBase.interceptors.response.use(
   (response) => {
     return response;
   },
   (error) => {
     if (error.response) {
       console.log("error.response", error?.response);
+      Alert.error({
+        content: error?.response?.data?.message,
+      });
     }
   }
 );
@@ -43,28 +47,28 @@ service.interceptors.response.use(
 const encodeURI = (url: string) =>
   encodeURIComponent(url).replace(/%2F/gi, "/");
 
-const apiService = {
+const api = {
   async get(url: string, config?: AxiosRequestConfig | undefined) {
-    const res = await service.get(encodeURI(url), config);
+    const res = await apiBase.get(encodeURI(url), config);
     return res?.data;
   },
   async post(url: string, data: any, config?: AxiosRequestConfig | undefined) {
-    const res = await service.post(encodeURI(url), data, config);
+    const res = await apiBase.post(encodeURI(url), data, config);
     return res?.data;
   },
   async put(url: string, data: any, config?: AxiosRequestConfig | undefined) {
-    const res = await service.put(encodeURI(url), data, config);
+    const res = await apiBase.put(encodeURI(url), data, config);
     return res?.data;
   },
   async patch(url: string, data: any, config?: AxiosRequestConfig | undefined) {
-    const res = await service.patch(encodeURI(url), data, config);
+    const res = await apiBase.patch(encodeURI(url), data, config);
     return res?.data;
   },
   async delete(url: string, config?: AxiosRequestConfig | undefined) {
-    const res = await service.delete(encodeURI(url), config);
+    const res = await apiBase.delete(encodeURI(url), config);
     return res?.data;
   },
 };
 
-export default apiService;
+export default api;
 export { axios, param, baseURL };
