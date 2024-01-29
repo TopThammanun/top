ff7eced6ba58d17523cc7413d4e026609f4cfca2
satang_budsai <satang_budsai@cmu.ac.th>
2024-01-29 17:15:26 +0700

    auto format save and set default background

--- src/layouts/root-layout.tsx
+++ src/layouts/root-layout.tsx
@@ -5,6 +5,7 @@ import { StateType } from '@/store'
 import Head from 'next/head'
 import React, { Fragment, ReactNode } from 'react'
 import { useSelector } from 'react-redux'
+import { Image } from '@nextui-org/react'
 
 type Props = {
     children: ReactNode
@@ -19,13 +20,19 @@ const RootLayout = (props: Props) => {
     return (
         <Fragment>
             <Head>
-                <title>{props.title || "DoubleN"}</title>
+                <title>{props.title || "DOUBLE NEXT TEMP"}</title>
                 <link rel="icon" href="/favicon.ico" />
                 <meta name="description" content={props.desc} />
                 <meta name="keyword" content={props.keyword} />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
             </Head>
             <main>
+                <Image
+                    src='/images/background-full.svg'
+                    removeWrapper
+                    radius='none'
+                    className='w-screen h-screen object-cover fixed -z-10'
+                />
                 <LoadingScreen isLoading={loaderState.loader > 0} />
                 {props.children}
             </main>
