5be97d612b1a84e117e75b194b9ef6b9de68a0ca
Top <thammanun_a@cmu.ac.th>
2024-03-06 17:19:22 +0700

    feature : pwa app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: false,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  }
+});
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
