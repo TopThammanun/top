6bc4425ecbb05f60303c224d872e0abd097433c2
Top <thammanun.a@chanwanich.digital>
2024-05-03 16:13:29 +0700

    add : sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://topthammanun.dev',
+    generateRobotsTxt: true,
+    generateIndexSitemap: false,
+    sitemapSize: 1000,
+}
